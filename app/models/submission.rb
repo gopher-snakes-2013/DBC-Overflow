64dd57cabff0a933fea25ec2fe135f151eca5a79
Apprentice <apprentice@dbc-29.local>
2013-10-31 14:40:50 -0700

    user can click a link to add a comment

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,5 @@
 class Submission < ActiveRecord::Base
+  attr_accessible :url, :title
   validates :title, presence: true
   validates :url, presence: true, format: {with: URI::regexp(%w(http https))}
   has_many :comments
--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -1,1 +1,21 @@
 require 'spec_helper'
+
+feature "Homepage" do
+  before(:each) do
+    @submission = Submission.create(url: "http://poop.com", title: "A Resource")
+    @comment = Comment.create(content: "all about poop", submission_id: 1)
+    visit root_path
+  end
+
+  context "user can add a comment" do
+    it "that shows under its submission" do
+      expect(page).to have_content("all about poop")
+    end
+
+    it "by clicking on a button" do
+      click_on "Add a Comment"
+      expect(current_path).to eq(new_comment_path)
+    end
+  end
+
+end
