9f36d41e8dd154e376efc481e3767d211b2d6d6f
Apprentice <apprentice@dbc-29.local>
2013-10-31 12:01:30 -0700

    added shoulda-matchers, tests for comment relationships

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,5 @@
 class Submission < ActiveRecord::Base
   validates :title, presence: true
   validates :url, presence: true, format: {with: URI::regexp(%w(http https))}
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131030175801) do
+ActiveRecord::Schema.define(:version => 20131031184821) do
+
+  create_table "comments", :force => true do |t|
+    t.string   "content"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.integer  "submission_id"
+  end
 
   create_table "submissions", :force => true do |t|
     t.string   "title"
