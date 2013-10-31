84058ee81670ba7c48da13fa4f09ca9f70c1cf56
B-Dog <bdog@BRANDONs-MacBook-Pro.local>
2013-10-31 12:34:26 -0700

    Tests for users controller all passing, created spec file for user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
 	has_many :submissions
+	validates_presence_of :username, :password
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,12 +5,16 @@ describe UsersController do
 		get :new
 		response.status.should eq(200)
 	end
-
 	context "#create" do
 		it 'creates a new user given proper params' do
 			expect {
-				post :create, user: { username: 'buffy', password: 'crazytime' } 
+				post :create, user: { username: 'buffy', password: 'crazytime' }
 			}.to change { User.count }.by(1)
+		end
+		it 'doesnt create a new user if its given invalid params' do
+			expect{
+				post :create, user: {username: 'new guy'}
+			}.to_not change { User.count }
 		end
 	end
 end
